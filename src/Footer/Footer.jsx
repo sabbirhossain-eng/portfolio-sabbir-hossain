3ba60b849c23121694ada6bee16e282539dc112e
Sabbir Hossain <sabbirh8181@gmail.com>
2024-03-05 03:45:23 +0600

    footer updates

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,57 +1,49 @@
-import { Typography } from "@material-tailwind/react";
 import { FaLinkedin } from "react-icons/fa6";
-import { TfiEmail, TfiWorld } from "react-icons/tfi";
+import { TfiWorld } from "react-icons/tfi";
 import { VscGithubInverted } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-[#c9f31d] py-6 text-center md:justify-between mt-10 mx-auto">
-      <Typography className="text-white text-xl font-medium flex items-center md:ml-10">
-            <TfiEmail className="mr-2"/> sabbirh8181@gmail.com
-          </Typography>
-      <ul className="flex flex-wrap items-center gap-y-2 gap-x-8 md:mr-10 mx-auto">
-        <li>
-          <Typography>
+    <footer className=" text-in-dark py-8 px-2 ">
+      <div className="flex flex-col items-center text-center">
+        <p className="text-lg">Stay connected with Me :</p>
+        <ul className="flex space-x-4 my-8">
+          <li>
             <Link
               to="https://www.linkedin.com/in/sabbirhossain1"
               target="_blank"
             >
-              <p className="text-white text-3xl font-semibold">
+              <p className=" text-3xl font-semibold hover:text-primary">
                 <FaLinkedin />
               </p>
             </Link>
-          </Typography>
-        </li>
-        <li>
-          <Typography
-            as="a"
-            href="#"
-            color="white"
-            className="font-normal transition-colors hover:text-[#c9f31d] focus:text-[#c9f31d]"
-          >
+          </li>
+          <li>
             <Link to="https://github.com/sabbirhossain-eng" target="_blank">
-              <p className="text-white text-3xl font-semibold">
+              <p className=" text-3xl font-semibold hover:text-primary">
                 <VscGithubInverted />
               </p>
             </Link>
-          </Typography>
-        </li>
-        <li>
-          <Typography>
+          </li>
+          <li>
             <Link to="https://sabbirhossain-8630c.web.app/" target="_blank">
-              <p className="text-white text-3xl font-semibold">
+              <p className=" text-3xl font-semibold hover:text-primary">
                 <TfiWorld />
               </p>
             </Link>
-          </Typography>
-        </li>
-      </ul>
+          </li>
+        </ul>
+        <p className="text-base">
+          Copyright &copy; 2024 - All right reserved by{" "}
+          <a
+            href="https://sabbirhossain-8630c.web.app"
+            className="hover:underline mx-2 text-primary"
+          >
+            Sabbir Hossain
+          </a>
+        </p>
       </div>
-      <Typography color="white" className="font-normal text-center">
-        &copy; 2023 Sabbir Hossain
-      </Typography>
     </footer>
   );
 };
